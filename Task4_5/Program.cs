5720da68b0b8193087d5d9e04e71d63eee00844f
shaburovaa@outlook.com <shaburovaa@outlook.com>
2025-03-05 13:20:22 +0300

    Выполнено д/з занятия 4. Спасибо!

---
+++ Task4_5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task4_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            do
+            {
+                Console.WriteLine("Введите число из отрезка [20; 60]: ");
+                a = Convert.ToInt32(Console.ReadLine());
+            }
+            while ((a < 20) || (a > 60));
+                        Console.WriteLine("Верно! 20 <= {0} <= 60", a);
+            Console.ReadKey();
+        }
+    }
+}
